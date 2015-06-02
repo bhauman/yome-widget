84cad4e41fb8acb9c53388a08e5111b8c06693a1
Bruce Hauman <bhauman@gmail.com>
2015-06-02 11:26:38 -0400

    change text

--- src/yome/core.cljs
+++ src/yome/core.cljs
@@ -566,7 +566,7 @@
     [:div.yome-widget-flex 
      [:div.yome-doors-windows-text 
      [:div.yome-graphic-image]
-      [:p "The walls of a Yome are made up of a series of upward and downward facing triangles (see illus). The diagram below represents the top plate (the plate between the top of the walls and the bottom of the roof). The diagram's corners represent the tips of the upward facing triangles and the edges represent the downward facing triangles."]
+      [:p "The walls of a Yome are made up of a series of upward and downward facing triangles (see side-bar illustration). The diagram below represents the top plate (the plate between the top of the walls and the bottom of the roof). The diagram's corners represent the tips of the upward facing triangles and the edges represent the downward facing triangles."]
       [:p "The doors and stovepipe vent are placed in upward triangles while the windows and large screen opening are placed in the downward triangles."]]]
     [:div.yome-svg-container 
     [:svg {:class "yome" :height 500 :width 500
